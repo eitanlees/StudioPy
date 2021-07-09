1b3af73d64f6dbb2d3699d7afbd1fee15d82ba57
ebrooker <18298060+ebrooker@users.noreply.github.com>
2021-07-09 09:33:04 -0400

    [reqs] Setting up a requirements directory to hold specific conda environment information. Currently there is a Windows 8.1 64-bit build, a no architecture build with just primary packages and the versions being used for development, and a no arch build with no versions that is the most risky and can be used to stress test development whenever any packages are updated (most likely PySimpleGUI).

--- iea/acs2/pdes/laplace_eqn.py
+++ iea/acs2/pdes/laplace_eqn.py
@@ -28,8 +28,8 @@ pi = np.pi
 class SubModuleWindow(BaseWindow):
 
   title="PDEs: Laplace Equation"
-
-  _npts = 20
+  _solver = "Jacobi"
+  _npts = 16
   _func = lambda junk,x,y: y*np.sin(pi*x**2) + x*np.cos(pi*y**2)
   _BCs = "Dirichlet"
   _levels = 10
@@ -41,16 +41,18 @@ class SubModuleWindow(BaseWindow):
 
   def _configure_layout(self):
     # sg.Theme("")
-
+    samp  = [16,32,64]
     txt_d = { "size":(30,1) }
     nxt_d = { "size":( 10,1), "key":"-NEXT-"  }
     exi_d = { "size":( 10,1), "key":"-EXIT-"  }
     fun_d = { "size":(31,1), "key":"-IFUNC-" }
-    trn_d = { "default_value":self._npts, "enable_events":True, "key":"-TRUNC-", "size":(4,5) }
+    smp_d = { "default_value":self._npts, "enable_events":True, "key":"-PNTS-",  "size":(4,5) }
     sub_d = { "bind_return_key":True,       "visible":False,      "key":"-SUBMIT-" }
     buttons = [
       [ sg.Text("", size=(1,1))                                                 ],
       [ sg.Button("Next - 0",  **nxt_d)                                             ],
+      [ sg.Text("", size=(1,1))                                                 ],
+      [ sg.Combo(samp, **smp_d), sg.Text("# of Mesh Points", **txt_d)        ],
       [ sg.Text("", size=(1,1))                                                 ],
       [ sg.Button("Exit", **exi_d), sg.Button("Submit", **sub_d),               ],
       ]
@@ -83,19 +85,26 @@ class SubModuleWindow(BaseWindow):
       self.window.close()
       return True
  
+    elif event == "-PNTS-":
+      self._npts = int(values["-PNTS-"])
+      self._grid()
+      self._ICs()
+      self._BCs()
+      self._draw()
+
     elif event == "-NEXT-":
-      while self._norm > 1e-3:
+      while self._norm > 1e-4 and self._cnt < 25:
         self._cnt+=1
         [self._laplace_step() for _ in range(10)]
         self._draw()
         self.window["-NEXT-"].update(f"Next - {self._cnt}")
 
 
-
   def _draw(self):
     # Plot both datasets
+    self.window["-NEXT-"].update(f"Next - {self._cnt}")
     plt.close("all")
-    plt.contourf(self._Xm, self._Ym, self._u, levels=self._contours, cmap="inferno")
+    plt.pcolormesh(self._Xm, self._Ym, self._u, cmap="jet", vmin=self._vmin, vmax=self._vmax)
 
     # Add any plot frills you want
     plt.ylim((0.0,1.0))
@@ -115,25 +124,15 @@ class SubModuleWindow(BaseWindow):
 
 
   def _ICs(self):
+    self._cnt = 0
+    self._norm = np.inf
     self._u = np.zeros((self._npts, self._npts))
     for i in range(self._npts):
       self._u[i,:] = self._func(self._x[i],self._y)
     self._BCs()
     self._u0 = self._u.copy()
-    self._contours = np.linspace(0.9*self._u.min(),1.1*self._u0.max(), self._levels)
-
-
-  def _laplace_step(self):
-    for i in range(1,self._npts-1):
-      for j in range(1,self._npts-1):
-        self._u[i,j] = 0.25 * ( self._u0[i+1,j] +\
-                                self._u0[i-1,j] +\
-                                self._u0[i,j+1] +\
-                                self._u0[i,j-1] )
-    self._BCs()
-    self._norm = la.norm(self._u-self._u0) / la.norm(self._u0)
-    self._u0 = self._u.copy()
-
+    self._vmin = 0.8 * self._u.min()
+    self._vmax = 1.2 * self._u.max()
 
   def _BCs(self):
     if self._BCs == 'Dirichlet':
@@ -141,6 +140,30 @@ class SubModuleWindow(BaseWindow):
       self._u[-1,:] = 0.5
       self._u[:,0] = 1.0
       self._u[:,-1] = np.sin(1.5*pi*x)
+
+
+  def _jacobi(self):
+    self._u[1:-1,1:-1] = 0.25 * (self._u[2:,1:-1] + self._u[0:-2,1:-1] +\
+                                self._u[1:-1,2:] + self._u[1:-1,0:-2])
+
+
+  def _gauss_seidel(self):
+
+    pass
+
+  def _laplace_step(self):
+    if self._solver=='Jacobi':
+      self._jacobi()
+    elif self._solver == "Gauss-Seidel":
+      self._gauss_seidel()
+    elif self._solver == "SOR":
+      self._sor()
+
+    self._BCs()
+    self._norm = la.norm(self._u-self._u0) / la.norm(self._u0)
+    self._u0 = self._u.copy()
+
+
 
 if __name__ == "__main__":
 
